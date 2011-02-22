943e8fdaccd837cb0ac33b830953d5055259cce0
Simon Cockell <sjcockell@gmail.com>
2011-02-22 11:24:49 +0000

    Changed archives.php for Google Scholar

--- trunk/themes/knowledgeblog/archives.php
+++ trunk/themes/knowledgeblog/archives.php
@@ -9,7 +9,7 @@ Template Name: Archives
 		<div id="content" class="single">
 			
 			<div class="post">
-				<h2>Етикети</h2>
+				<h2>Tag Cloud</h2>
 				
 				<p class="tag-cloud">
 				<?php wp_tag_cloud('smallest=1&largest=4&unit=em'); ?>
@@ -21,19 +21,19 @@ Template Name: Archives
 
 			<?php /* include (TEMPLATEPATH . '/searchform.php');*/ ?>
 
-			<h2>Архив по месеци</h2>
+			<h2>Yearly Article Archive</h2>
 			<ul>
-				<?php wp_get_archives('type=monthly'); ?>
+				<?php wp_get_archives('type=yearly'); ?>
 			</ul>
 			</div>
 	
-			<div class="post column right">
+			<!--div class="post column right">
 
 			<h2>Архив по категории</h2>
 			<ul>
 				 <?php wp_list_categories('title_li='); ?>
 			</ul>
-			</div>
+			</div-->
 
 		</div>
 
