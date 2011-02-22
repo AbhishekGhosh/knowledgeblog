9c6e7869691efbdfd8f12c25618d2262ca7fa310
Simon Cockell <sjcockell@gmail.com>
2011-02-22 11:29:51 +0000

    Minor archives.php revision

--- trunk/themes/knowledgeblog/archives.php
+++ trunk/themes/knowledgeblog/archives.php
@@ -6,17 +6,8 @@ Template Name: Archives
 
 <?php get_header(); ?>
 
+
 		<div id="content" class="single">
-			
-			<div class="post">
-				<h2>Tag Cloud</h2>
-				
-				<p class="tag-cloud">
-				<?php wp_tag_cloud('smallest=1&largest=4&unit=em'); ?>
-				</p>
-
-			</div>
-
 			<div class="post column left">
 
 			<?php /* include (TEMPLATEPATH . '/searchform.php');*/ ?>
@@ -25,6 +16,15 @@ Template Name: Archives
 			<ul>
 				<?php wp_get_archives('type=yearly'); ?>
 			</ul>
+			</div>
+			
+			<div class="post">
+				<h2>Tag Cloud</h2>
+				
+				<p class="tag-cloud">
+				<?php wp_tag_cloud('smallest=1&largest=4&unit=em'); ?>
+				</p>
+
 			</div>
 	
 			<!--div class="post column right">
