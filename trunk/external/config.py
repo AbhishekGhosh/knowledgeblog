c1ef789229e4eaffe04df82c614e3384b37c6957
Simon Cockell <sjcockell@gmail.com>
2011-05-24 10:18:05 +0100

    Overhaul of external dependencies fetch code

---
+++ trunk/external/config.py
@@ -0,0 +1,26 @@
+"""Configure options for fetch_external_plugins"""
+config = {
+    #define the root directory of your Wordpress/Knowledgeblog installation
+    'wordpress_root':'',
+    #plugins from wordpress.org, not written by kblog team
+    'external_dependencies': ['co-authors-plus',
+                'post-revision-display',
+                'wp-post-to-pdf',
+                'edit-flow',
+                'epub-export',
+                'syntaxhighlighter',
+                'wp-gravatar',
+                'wordpress-table-of-contents',
+                ],
+    #plugins from wordpress.org, written by kblog team
+    'knowledgeblog_dependencies': ['kcite',
+                'mathjax-latex',
+                'knowledgeblog-table-of-contents',
+                ],
+    #base url for plugin downloading
+    'root_url': 'http://downloads.wordpress.org/plugin/',
+    #which plugins do you not want kept at the latest version?
+    #and what version do you wish to maintain them at?
+    #key:value e.g.: 'kcite':'1.0'
+    'override_versions':{},
+}
